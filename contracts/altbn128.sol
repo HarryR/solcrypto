3fa33b881c3dae909245dda7850ae79ff37aaad5
HarryR <HarryR@users.noreply.github.com>
2018-01-01 06:55:52 +0000

    Initial commit

---
+++ contracts/altbn128.sol
@@ -0,0 +1,260 @@
+// This file is MIT Licensed.
+//
+// From: https://gist.githubusercontent.com/chriseth/f9be9d9391efc5beb9704255a8e2989d/raw/4d0fb90847df1d4e04d507019031888df8372239/snarktest.solidity
+// 
+// Copyright 2017 Christian Reitwiessner
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+pragma solidity ^0.4.14;
+
+library altbn128 {
+   	// p = p(u) = 36u^4 + 36u^3 + 24u^2 + 6u + 1
+    uint256 internal constant FIELD_ORDER = 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47;
+
+    // Number of elements in the field (often called `q`)
+    // n = n(u) = 36u^4 + 36u^3 + 18u^2 + 6u + 1
+    uint256 internal constant GEN_ORDER = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001;
+
+    uint256 internal constant CURVE_B = 3;
+
+    // a = (p+1) / 4
+    uint256 internal constant CURVE_A = 0xc19139cb84c680a6e14116da060561765e05aa45a1c72a34f082305b61f3f52;
+
+	struct G1Point {
+		uint X;
+		uint Y;
+	}
+
+	// Encoding of field elements is: X[0] * z + X[1]
+	struct G2Point {
+		uint[2] X;
+		uint[2] Y;
+	}
+
+	// (P+1) / 4
+	function A() pure internal returns (uint256) {
+		return CURVE_A;
+	}
+
+	function P() pure internal returns (uint256) {
+		return FIELD_ORDER;
+	}
+
+	function N() pure internal returns (uint256) {
+		return GEN_ORDER;
+	}
+
+	/// @return the generator of G1
+	function P1() pure internal returns (G1Point) {
+		return G1Point(1, 2);
+	}
+
+	function HashToPoint(bytes32 s)
+        internal constant returns (G1Point)
+    {
+        uint256 beta = 0;
+        uint256 y = 0;
+
+        // XXX: Gen Order (n) or Field Order (p) ?
+        uint256 x = uint256(s) % GEN_ORDER;
+
+        while( true ) {
+            (beta, y) = FindYforX(x);
+
+            // y^2 == beta
+            if( beta == mulmod(y, y, FIELD_ORDER) ) {
+                return G1Point(x, y);
+            }
+
+            x = addmod(x, 1, FIELD_ORDER);
+        }
+    }
+
+
+    /**
+    * Given X, find Y
+    *
+    *   where y = sqrt(x^3 + b)
+    *
+    * Returns: (x^3 + b), y
+    */
+    function FindYforX(uint256 x)
+        internal constant returns (uint256, uint256)
+    {
+        // beta = (x^3 + b) % p
+        uint256 beta = addmod(mulmod(mulmod(x, x, FIELD_ORDER), x, FIELD_ORDER), CURVE_B, FIELD_ORDER);
+
+        // y^2 = x^3 + b
+        // this acts like: y = sqrt(beta)
+        uint256 y = expMod(beta, CURVE_A, FIELD_ORDER);
+
+        return (beta, y);
+    }
+
+
+    // a - b = c;
+    function submod(uint a, uint b) internal pure returns (uint){
+        uint a_nn;
+
+        if(a>b) {
+            a_nn = a;
+        } else {
+            a_nn = a+GEN_ORDER;
+        }
+
+        return addmod(a_nn - b, 0, GEN_ORDER);
+    }
+
+
+    function expMod(uint256 _base, uint256 _exponent, uint256 _modulus)
+        internal constant returns (uint256 retval)
+    {
+        bool success;
+        uint256[1] memory output;
+        uint[6] memory input;
+        input[0] = 0x20;        // baseLen = new(big.Int).SetBytes(getData(input, 0, 32))
+        input[1] = 0x20;        // expLen  = new(big.Int).SetBytes(getData(input, 32, 32))
+        input[2] = 0x20;        // modLen  = new(big.Int).SetBytes(getData(input, 64, 32))
+        input[3] = _base;
+        input[4] = _exponent;
+        input[5] = _modulus;
+        assembly {
+            success := staticcall(sub(gas, 2000), 5, input, 0xc0, output, 0x20)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid }
+        }
+        require(success);
+        return output[0];
+    }
+
+
+	/// @return the generator of G2
+	function P2() pure internal returns (G2Point) {
+		return G2Point(
+			[11559732032986387107991004021392285783925812861821192530917403151452391805634,
+			 10857046999023057135944570762232829481370756359578518086990519993285655852781],
+			[4082367875863433681332203403145435568316851327593401208105741076214120093531,
+			 8495653923123431417604973247489272438418190587263600148770280649306958101930]
+		);
+	}
+
+	/// @return the negation of p, i.e. p.add(p.negate()) should be zero.
+	function g1neg(G1Point p) pure internal returns (G1Point) {
+		// The prime q in the base field F_q for G1
+		uint q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+		if (p.X == 0 && p.Y == 0)
+			return G1Point(0, 0);
+		return G1Point(p.X, q - (p.Y % q));
+	}
+
+	/// @return the sum of two points of G1
+	function g1add(G1Point p1, G1Point p2) constant internal returns (G1Point r) {
+		uint[4] memory input;
+		input[0] = p1.X;
+		input[1] = p1.Y;
+		input[2] = p2.X;
+		input[3] = p2.Y;
+		bool success;
+		assembly {
+			success := staticcall(sub(gas, 2000), 6, input, 0xc0, r, 0x60)
+			// Use "invalid" to make gas estimation work
+			switch success case 0 { invalid }
+		}
+		require(success);
+	}
+
+	/// @return the product of a point on G1 and a scalar, i.e.
+	/// p == p.mul(1) and p.add(p) == p.mul(2) for all points p.
+	function g1mul(G1Point p, uint s) constant internal returns (G1Point r) {
+		uint[3] memory input;
+		input[0] = p.X;
+		input[1] = p.Y;
+		input[2] = s;
+		bool success;
+		assembly {
+			success := staticcall(sub(gas, 2000), 7, input, 0x80, r, 0x60)
+			// Use "invalid" to make gas estimation work
+			switch success case 0 { invalid }
+		}
+		require (success);
+	}
+
+	/// @return the result of computing the pairing check
+	/// e(p1[0], p2[0]) *  .... * e(p1[n], p2[n]) == 1
+	/// For example pairing([P1(), P1().negate()], [P2(), P2()]) should
+	/// return true.
+	function pairing(G1Point[] p1, G2Point[] p2) constant internal returns (bool) {
+		require(p1.length == p2.length);
+		uint elements = p1.length;
+		uint inputSize = elements * 6;
+		uint[] memory input = new uint[](inputSize);
+		for (uint i = 0; i < elements; i++)
+		{
+			input[i * 6 + 0] = p1[i].X;
+			input[i * 6 + 1] = p1[i].Y;
+			input[i * 6 + 2] = p2[i].X[0];
+			input[i * 6 + 3] = p2[i].X[1];
+			input[i * 6 + 4] = p2[i].Y[0];
+			input[i * 6 + 5] = p2[i].Y[1];
+		}
+		uint[1] memory out;
+		bool success;
+		assembly {
+			success := staticcall(sub(gas, 2000), 8, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+			// Use "invalid" to make gas estimation work
+			switch success case 0 { invalid }
+		}
+		require(success);
+		return out[0] != 0;
+	}
+
+	/// Convenience method for a pairing check for two pairs.
+	function pairingProd2(G1Point a1, G2Point a2, G1Point b1, G2Point b2) constant internal returns (bool) {
+		G1Point[] memory p1 = new G1Point[](2);
+		G2Point[] memory p2 = new G2Point[](2);
+		p1[0] = a1;
+		p1[1] = b1;
+		p2[0] = a2;
+		p2[1] = b2;
+		return pairing(p1, p2);
+	}
+
+	/// Convenience method for a pairing check for three pairs.
+	function pairingProd3(
+			G1Point a1, G2Point a2,
+			G1Point b1, G2Point b2,
+			G1Point c1, G2Point c2
+	) constant internal returns (bool) {
+		G1Point[] memory p1 = new G1Point[](3);
+		G2Point[] memory p2 = new G2Point[](3);
+		p1[0] = a1;
+		p1[1] = b1;
+		p1[2] = c1;
+		p2[0] = a2;
+		p2[1] = b2;
+		p2[2] = c2;
+		return pairing(p1, p2);
+	}
+
+	/// Convenience method for a pairing check for four pairs.
+	function pairingProd4(
+			G1Point a1, G2Point a2,
+			G1Point b1, G2Point b2,
+			G1Point c1, G2Point c2,
+			G1Point d1, G2Point d2
+	) constant internal returns (bool) {
+		G1Point[] memory p1 = new G1Point[](4);
+		G2Point[] memory p2 = new G2Point[](4);
+		p1[0] = a1;
+		p1[1] = b1;
+		p1[2] = c1;
+		p1[3] = d1;
+		p2[0] = a2;
+		p2[1] = b2;
+		p2[2] = c2;
+		p2[3] = d2;
+		return pairing(p1, p2);
+	}
+}
